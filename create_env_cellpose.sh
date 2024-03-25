8b3cf92dcc02e749c698d6c62e10cfd105c1c60a
jcopperm <jcopperm@gmail.com>
2024-03-25 13:19:27 -0700

    cellpose

--- create_env_cellpose.sh
+++ create_env_cellpose.sh
@@ -3,7 +3,7 @@ CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 conda create --name cellpose python=3.8 -y
 conda activate cellpose
-python -m pip install cellpose --upgrade
+python -m pip install cellpose==2.3.2
 pip uninstall torch -y
 conda install pytorch==1.12.0 cudatoolkit=11.3 -c pytorch -y
 conda env update --file /home/groups/CEDAR/copperma/env_scripts/environment_cellpose.yml

b02c54c85d68b34fb64f373ff140b82e8dad9820
coppermanj <copperma@ohsu.edu>
2025-01-15 15:46:26 -0800

    edited ilastik_env

--- create_env_ilastik_env.sh
+++ create_env_ilastik_env.sh
@@ -1,6 +1,6 @@
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
-conda env create --file ilastik_env_packages.txt
+conda create --name ilastik_env --file ilastik_env_packages.txt
 bash
 conda activate ilastik_env
 conda install -c anaconda ipykernel

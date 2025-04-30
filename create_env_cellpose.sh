093c44b0a732d732d9ff19bb85b549de937a16f0
coppermanj <copperma@ohsu.edu>
2025-04-30 10:32:28 -0700

    cellpose env addtl

--- create_env_cellpose.sh
+++ create_env_cellpose.sh
@@ -4,7 +4,7 @@ source $CONDA_BASE/etc/profile.d/conda.sh
 conda create --name cellpose python=3.8 -y
 conda activate cellpose
 #python -m pip install cellpose --upgrade
-python -m pip install cellpose==2.2.2 --upgrade
+python -m pip install cellpose==3.1.0 #--upgrade
 #python -m pip install cellpose==2.3.2
 pip uninstall torch -y
 conda install pytorch=1.12.0 cudatoolkit=11.3 mkl=2024.0 -c pytorch -y

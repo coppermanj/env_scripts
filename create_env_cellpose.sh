d4ff16972ef5921dfda2ab3a3ddad83e46ddfe81
coppermanj <copperma@ohsu.edu>
2025-05-15 13:47:24 -0700

    updated torch https://forum.image.sc/t/cellpose-3-1-0-throwing-type-error/104415/8

--- create_env_cellpose.sh
+++ create_env_cellpose.sh
@@ -8,7 +8,8 @@ python -m pip install cellpose==3.1.0 #--upgrade
 #python -m pip install cellpose==2.3.2
 pip uninstall torch -y
 conda config --add channels conda-forge
-conda install pytorch=1.12.0 cudatoolkit=11.3 mkl=2024.0 -c pytorch -y
+#conda install pytorch=1.12.0 cudatoolkit=11.3 mkl=2024.0 -c pytorch -y
+conda install pytorch pytorch-cuda=11.8 -c pytorch -c nvidia
 conda env update --file environment_cellpose.yml
 conda install -c anaconda ipykernel
 python -m ipykernel install --user --name=cellpose
